e1879781bd076641d5325141144276e5ab9e5692
SalC124 <Saltcal124@gmail.com>
2024-09-06 15:57:15 -0400

    Day 2

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,8 +1,31 @@
-const App = () => {
+const ReactLogo = (props) => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <img
+      src="https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png"
+      alt="React Logo"
+      width={props.size}
+    />
+  )
+}
+
+const App = () => {
+  const releaseYear = 2013;
+  const developerCompany = "Facebook, Inc.";
+  console.log("Release Year: ", releaseYear);
+  console.log(`Developer Company: ${developerCompany}`);
+  console.log("hello from App component")
+  return (
+    <>
+    <h1>Hello React!!!</h1>
+    <ReactLogo size="100"/>
+    <ReactLogo size="75"/>
+    <ReactLogo size="50"/>
+    <ReactLogo size="25"/>
+      <p>
+        React was developed by {developerCompany} and was first released{" "}
+        {releaseYear}, {new Date().getFullYear() - releaseYear} years ago.
+      </p>
+    </>
   )
 }
 
