10247c628867b46237edebfbcfa120da37fc08d7
SalC124 <saltcal124@gmail.com>
2024-10-08 15:50:24 -0400

    Day 7 (ask Mr. Sen why it doesn't work properly)

--- chapter 2/movies-app/src/App.jsx
+++ chapter 2/movies-app/src/App.jsx
@@ -5,10 +5,23 @@ import { useState } from "react";
 const App = ({ movies }) => {
   const [movieList, setMovieList] = useState(movies);
   const [movieName, setMovieName] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const filteredList = showAll
+    ? movieList
+    : movieList.filter((m) => m.watchList); // m gives the movies
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newMovieObj = {
+      id: Date.now(),
+      title: movieName,
+      watchList: false,
+    };
     console.log("refresh prevented");
+    movieList.concat(newMovieObj);
+    console.log(movieList.concat(newMovieObj));
+    setMovieName("");
   };
 
   const handleChange = (e) => {
@@ -23,8 +36,9 @@ const App = ({ movies }) => {
         <input type="text" value={movieName} onChange={handleChange} />
         <button type="submit">Add Movie</button>
       </form>
-      <h2>Movie List</h2>
-      {movies.map((movie) => {
+      <button onClick={() => setShowAll(!showAll)}>Toggle Watchlist</button>
+      {showAll ? <h2>Movie List</h2> : <h2>Your Watchlist</h2>}
+      {filteredList.map((movie) => {
         return <Movie key={Movie.id} movie={movie} />;
       })}
     </div>
