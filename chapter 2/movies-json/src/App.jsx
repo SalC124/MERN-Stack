22d4fb18d0d3beec7a1284aafaf3c46d9a635e0f
SalC124 <saltcal124@gmail.com>
2024-10-15 16:09:36 -0400

    ITS WORKING. DATA PERSISTS USING THE BACKEND (Day 8)

--- chapter 2/movies-json/src/App.jsx
+++ chapter 2/movies-json/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Movie from "./components/Movie.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [movieName, setMovieName] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const filteredList = showAll ? movies : movies.filter((m) => m.watchList);
+  // const moviesData = axios.get("http://localhost:3001/movies");
+  // console.log(moviesData);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/movies").then((res) => {
+      setMovies(res.data);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMovieObj = {
+      title: movieName,
+      watchList: false,
+    };
+    axios.post("http://localhost:3001/movies", newMovieObj).then((res) => {
+      setMovies(movies.concat(res.data));
+    });
+    // setMovieList([...movieList, newMovieObj]);
+    setMovies(movies.concat(newMovieObj));
+    setMovieName("");
+    // console.log("New Object: ", newMovieObj);
+  };
+
+  const handleChange = (e) => {
+    setMovieName(e.target.value);
+    // console.log(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Movies Application</h1>
+      <h2>Add a New Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={movieName} onChange={handleChange} />
+        <button type="submit">Add Movie</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        {showAll ? "Show Watchlist Only" : "Show All Movies"}
+      </button>
+      {showAll ? <h2>All Movies</h2> : <h2>Your Watchlist</h2>}
+      {filteredList.map((movie) => {
+        return <Movie key={movie.id} movie={movie} />;
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
