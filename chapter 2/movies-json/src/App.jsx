da248bdb2bcc8a739715e51c370395a0128c4f07
SalC124 <saltcal124@gmail.com>
2024-10-25 16:19:38 -0400

    Day 10

--- chapter 2/movies-json/src/App.jsx
+++ chapter 2/movies-json/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import movieService from "./services/movies";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [movieName, setMovieName] = useState("");
   const [releaseYear, setReleaseYear] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [notification, setNotification] = useState(null);
 
   const filteredList = showAll ? movies : movies.filter((m) => m.watchList);
   // const moviesData = axios.get("http://localhost:3001/movies");
@@ -16,11 +18,8 @@ const App = () => {
     // axios.get("http://localhost:3001/movies").then((res) => {
     //   setMovies(res.data);
     // });
-
-    // can only handle the promise with then
     movieService.getMovies().then((data) => setMovies(data));
   }, []);
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,14 +28,12 @@ const App = () => {
       watchList: false,
       year: releaseYear,
     };
-
     movieService.addMovie(newMovieObj).then((data) => {
-      setMovies(movies.concat(data))
+      setMovies(movies.concat(data));
     });
     // axios.post("http://localhost:3001/movies", newMovieObj).then((res) => {
     //   setMovies(movies.concat(res.data));
     // });
-
     // setMovieList([...movieList, newMovieObj]);
     // setMovies(movies.concat(newMovieObj));
     setMovieName("");
@@ -51,14 +48,22 @@ const App = () => {
     //   .then((res) =>
     //     setMovies(movies.map((m) => (m.id !== res.data.id ? m : res.data)))
     //   );
-    movieService.updateMovie(updatedMovie)
-      .then(data => {
+    movieService
+      .updateMovie(updatedMovie)
+      .then((data) => {
         setMovies(movies.map((m) => (m.id !== data.id ? m : data)));
-      }).catch(e=>{
-        console.log("Error ", e);
-        alert(`The movie "${movie.title}" is already removed from the server`);
+      })
+      .catch((e) => {
+        console.log("Error: ", e);
+        setNotification(
+          `The movie "${movie.title}" is already removed from the server`
+        );
+        setTimeout(() => {
+          setNotification(null);
+        }, 5000);
+        // alert(`The movie "${movie.title}" is already removed from the server`);
         setMovies(movies.filter((m) => m.id !== movie.id));
-    })
+      });
   };
 
   const handleChange = (e) => {
@@ -69,6 +74,7 @@ const App = () => {
   return (
     <div>
       <h1>Movies Application</h1>
+      <Notification notification={notification} />
       <h2>Add a New Movie</h2>
       <form onSubmit={handleSubmit}>
         <input
